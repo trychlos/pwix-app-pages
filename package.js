fd4f0ef81716bd01be3b1097fcac0c18d6f99ced
Pierre Wieser <p.wieser@trychlos.org>
2026-05-03 18:28:41 +0200

    Post-release minimal version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'pwix:app-pages',
-    version: '1.3.1',
+    version: '1.3.2-rc.0',
     summary: 'Meteor APP pages description',
     git: 'https://github.com/trychlos/pwix-app-pages.git',
     documentation: 'README.md'
